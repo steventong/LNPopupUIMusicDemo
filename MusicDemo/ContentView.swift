91fb273d688eef12f5a35822f6222a6f73b3f4ab
Steven <4138760+steventong@users.noreply.github.com>
2024-10-05 17:06:30 +0800

    initial commit

--- MusicDemo/ContentView.swift
+++ MusicDemo/ContentView.swift
@@ -5,17 +5,65 @@
 //  Created by Steven on 2024/10/5.
 //
 
+import LNPopupUI
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var musicPlayerEnv = MusicPlayerEnv()
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Text("Timer: \(musicPlayerEnv.progress)")
+                .font(.largeTitle)
+            
+            Button(action: {
+                musicPlayerEnv.startOrPauseTimer()
+            }, label: {
+                Text(musicPlayerEnv.isPlaying ? "pause" : "play")
+                    .background(.red)
+                    .font(.largeTitle)
+            })
         }
-        .padding()
+        .environmentObject(musicPlayerEnv)
+        .popup(isBarPresented: $musicPlayerEnv.isBarPresented, isPopupOpen: $musicPlayerEnv.isPopupOpen) {
+            // player view
+            PlayerView()
+                .environmentObject(musicPlayerEnv)
+                .popupTitle(musicPlayerEnv.title, subtitle: musicPlayerEnv.subTitle)
+        }
+        .popupBarStyle(.floating)
+        .popupInteractionStyle(.customizedSnap(percent: 0.2))
+        .popupCloseButtonStyle(.none)
+        .popupBarProgressViewStyle(.none)
+    }
+}
+
+struct PlayerView: View {
+    @State var timer: Double = 0.0
+//    @EnvironmentObject var musicPlayerEnv: MusicPlayerEnv
+
+    var body: some View {
+        VStack {
+            Spacer()
+
+//            Text("Timer: \(musicPlayerEnv.progress)")
+
+//            Button(action: {
+//                musicPlayerEnv.startOrPauseTimer()
+//            }, label: {
+//                Text(musicPlayerEnv.isPlaying ? "pause" : "play")
+//                    .background(.red)
+//            })
+
+            Spacer()
+
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.blue)
     }
 }
 
---
+++ MusicDemo/MusicPlayerEnv.swift
@@ -0,0 +1,44 @@
+//
+//  MusicPlayerEnv.swift
+//  MusicDemo
+//
+//  Created by Steven on 2024/10/5.
+//
+
+import Combine
+import Foundation
+import SwiftUI
+
+class MusicPlayerEnv: ObservableObject {
+    @Published var isBarPresented = true
+    @Published var isPopupOpen = false
+    @Published var progress: Double = 0
+
+    @Published var title = "Title"
+    @Published var subTitle = "SubTitle"
+
+    @Published var isPlaying = false
+    @Published var timerCancellable: Cancellable?
+
+    func startOrPauseTimer() {
+        if isPlaying {
+            stopTimer()
+        } else {
+            startTimer()
+        }
+    }
+
+    func startTimer() {
+        isPlaying = true
+        timerCancellable = Timer.publish(every: 0.1, on: .main, in: .common)
+            .autoconnect()
+            .sink { _ in
+                self.progress += 0.1
+            }
+    }
+
+    func stopTimer() {
+        isPlaying = false
+        timerCancellable?.cancel()
+    }
+}
