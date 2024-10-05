7f1b47184cdefaab461023c25fb11cbdb416792e
Steven <4138760+steventong@users.noreply.github.com>
2024-10-05 20:00:22 +0800

    update demo

--- MusicDemo/ContentView.swift
+++ MusicDemo/ContentView.swift
@@ -9,57 +9,65 @@ import LNPopupUI
 import SwiftUI
 
 struct ContentView: View {
-    
     @EnvironmentObject var musicPlayerEnv: MusicPlayerEnv
 
     var body: some View {
-        VStack {
-            Text("Timer: \(musicPlayerEnv.progress)")
+        print("the root view is being redrawn")
+
+        return VStack {
+            Text("Root View")
                 .font(.largeTitle)
                 .padding(20)
-
-            Button(action: {
-                musicPlayerEnv.startOrPauseTimer()
-            }, label: {
-                Text(musicPlayerEnv.isPlaying ? "pause" : "play")
-                    .background(.red)
-                    .font(.largeTitle)
-            })
         }
         .popup(isBarPresented: $musicPlayerEnv.isBarPresented,
                isPopupOpen: $musicPlayerEnv.isPopupOpen) {
-            
             // player view
             PlayerView()
                 .popupTitle(musicPlayerEnv.title, subtitle: musicPlayerEnv.subTitle)
-            
-        } .popupBarStyle(.floating)
-            .popupInteractionStyle(.customizedSnap(percent: 0.2))
-            .popupCloseButtonStyle(.none)
-            .popupBarProgressViewStyle(.none)
-       
+        }
+        .popupBarStyle(.floating)
+        .popupInteractionStyle(.customizedSnap(percent: 0.2))
+        .popupCloseButtonStyle(.none)
+        .popupBarProgressViewStyle(.none)
     }
 }
 
 struct PlayerView: View {
-    
     @EnvironmentObject var musicPlayerEnv: MusicPlayerEnv
-    
+    @EnvironmentObject var musicPlayerTimerEnv: MusicPlayerTimerEnv
+
     var body: some View {
-        VStack {
+        print("the player view is being redrawn")
+
+        return VStack {
             Spacer()
 
-            Text("Music Player")
+            Text("Player View")
                 .font(.largeTitle)
+
+            Text("Timer: \(musicPlayerTimerEnv.progress)")
+                .font(.largeTitle)
+                .padding(20)
+
+            Button(action: {
+                musicPlayerTimerEnv.startOrPauseTimer()
+            }, label: {
+                Text(musicPlayerTimerEnv.isPlaying ? "PAUSE" : "PLAY")
+                    .background(.red)
+                    .font(.largeTitle)
+                    .padding()
+
+            })
 
             Button(action: {
                 musicPlayerEnv.isPopupOpen = false
             }, label: {
-                Text("Close")
+                Text("Close Player View")
                     .background(.green)
-                    .font(.largeTitle)
+                    .font(.title)
+                    .padding()
             })
-            
+
             Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
--- MusicDemo/MusicDemoApp.swift
+++ MusicDemo/MusicDemoApp.swift
@@ -10,11 +10,13 @@ import SwiftUI
 @main
 struct MusicDemoApp: App {
     @StateObject private var musicPlayerEnv = MusicPlayerEnv()
+    @StateObject private var musicPlayerTimerEnv = MusicPlayerTimerEnv()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(musicPlayerEnv)
+                .environmentObject(musicPlayerTimerEnv)
         }
     }
 }
